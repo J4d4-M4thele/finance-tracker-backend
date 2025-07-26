22dddd55be3a6711c2b842a6b7b3ee130a611908
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-07-26 18:41:41 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import financialRecordRouter from "./routes/financial-records";
+import dotenv from "dotenv";
+import cors from "cors";
+
+const app: Express = express();
+
+dotenv.config();
+
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors());
+
+const mongoURI: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@testcluster1.iqkh70e.mongodb.net/${process.env.MONGO_DATABASE}`;
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("Successfully connected to database!"))
+  .catch((err) => console.error("Failed to connect to database:", err));
+
+app.use("/financial-records", financialRecordRouter);
+
+app.listen(port, () => {
+  console.log(`Server Running on port: ${port}`);
+});
